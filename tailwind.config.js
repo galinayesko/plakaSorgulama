c4444b7cc14da43236ec3cb400d6fd7ada58d797
galina yesko <galina_yesko@hotmail.com>
2022-12-16 21:03:04 +0300

    plaka sorgulama paneli

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  content: ['./*.html'],
+  theme: {},
+    extend: {
+      fontFamily: {
+        sans: ['Josefin Sans', 'sans-serif'],
+        alata: ['Alata'],
+    },
+  },
+  plugins: [],
+}
